eba130ada5e8affcdfdd5991f5ab6b74a63c2378
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-05 20:54:25 +0700

    Create  [Comparator Lower_bound Upper_bound]. Bài 15. Lớn hơn.java

---
+++ [Comparator Lower_bound Upper_bound]. Bài 15. Lớn hơn.java
@@ -0,0 +1,43 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static int firstPos(int []a, int l ,int r,int x){
+        int res = -1;
+        while (l <= r)
+        {
+            int m = (int)(l + r) / 2;
+            if (a[m] > x) {
+                res = m;
+                r = m - 1;
+            }
+            else {
+                l = m + 1;
+            }
+        }
+        return res;
+    }
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt(); int m = sc.nextInt();
+        int []a = new int[n];
+        int []b = new int[m];
+        for(int i = 0; i < n;i++){
+            a[i] = sc.nextInt();
+        }
+        for(int i = 0; i < m;i++){
+            b[i] = sc.nextInt();
+        }
+        Arrays.sort(b);
+        for(int i : a){
+            int pos = firstPos(b,0,m-1,i);
+            if (pos == -1) {
+                System.out.print(0 + " ");
+            }
+            else {
+                System.out.print((m - pos) + " ");
+            }
+        }
+    }
+}
