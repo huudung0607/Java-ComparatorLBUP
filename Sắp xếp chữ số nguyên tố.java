4a93df3339f79b1fd59dfedfcfd2952bcb20a117
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-12 09:38:21 +0700

    Create Sắp xếp chữ số nguyên tố.java

---
+++ Sắp xếp chữ số nguyên tố.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    public static int dem(long n){
+        int cnt = 0;
+        while(n != 0){
+            long last = n % 10;
+            if(last == 2 || last == 3 || last == 5 || last == 7 ){
+                cnt++;
+            }
+            n/=10;
+        }
+        return cnt;
+    }
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        Long []a = new Long[n];
+        for(int i = 0; i < n ;i++){
+            a[i] = sc.nextLong();
+        }
+        Arrays.sort(a, new Comparator<Long>() {
+            @Override
+            public int compare(Long o1, Long o2) {
+                return dem(o2) - dem(o1);
+            }
+        });
+        for(var i : a){
+            System.out.print(i + " ");
+        }
+    }
+}
