6bf062e7150b500b088d9357177ee214b46a64d6
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-13 09:29:06 +0700

    Create Lớn hơn hoặc bằng.java

---
+++ Lớn hơn hoặc bằng.java
@@ -0,0 +1,36 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    public static int firstPosition(int []a, int l ,int r ,int x){
+        int res = -1;
+        while(l <= r){
+            int m = (int)(l + r)/2;
+            if(a[m] >= x){
+                res = m;
+                r = m - 1;
+            }
+            else l = m + 1;
+        }
+        return res;
+    }
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt(), m = sc.nextInt();
+        int []a = new int[n];
+        int []b = new int[m];
+        for(int i = 0 ;i < n;i++){
+            a[i] = sc.nextInt();
+        }
+        for(int i = 0; i < m;i++){
+            b[i] = sc.nextInt();
+        }
+        Arrays.sort(b);
+        for(var i : a){
+            int pos = firstPosition(b,0,m - 1,i);
+            if(pos == -1) System.out.print(0 + " ");
+            else System.out.print(m - pos + " ");
+        }
+    }
+}
