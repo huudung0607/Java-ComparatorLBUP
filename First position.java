206065768e1a0e5e9c035d08da7bdb3ca116431c
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-09-13 09:12:50 +0700

    Create First position.java

---
+++ First position.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+public class Main {
+    public static int firstPosition(int []a, int l ,int r ,int x){
+        int res = -1;
+        while(l <= r){
+            int m = (int)(l + r)/2;
+            if(a[m] == x){
+                res = m;
+                r = m - 1;
+            }
+            else if(a[m] > x){
+                r = m - 1;
+            }
+            else l = m + 1;
+        }
+        return res;
+    }
+    private static int mod = 1000000007;
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt(), k = sc.nextInt();
+        int []a = new int[n];
+        for(int i = 0 ;i < n;i++){
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+        int pos = firstPosition(a,0,n - 1,k);
+        if(pos == -1) System.out.println(-1);
+        else System.out.print(pos);
+    }
+}
